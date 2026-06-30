2f3dab5db7d488c3939473565600eae88e00a8c9
Soumyajit Bala <soumyajitbala409@gmail.com>
2026-07-01 00:29:29 +0530

    initial commit

---
+++ 01_Basics/01_hello_world.c
@@ -0,0 +1,12 @@
+/* 
+ First program in C
+ This program prints "Hello World" on the screen.
+*/
+
+#include<stdio.h>
+    int main() {
+        
+        printf("Hello World!");
+
+    return 0;
+}
