84ac8afa6926f8b2b0c89251f20fa9a0b023e76c
Soumyajit Bala <soumyajitbala409@gmail.com>
2026-07-01 01:02:26 +0530

    re-commiting 01 and 02 program

---
+++ 01_Basics/02_sum.c
@@ -0,0 +1,18 @@
+/* 
+ Program to calculate the sum of two numbers
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Display sum
+    printf("The sum is: %d\n", a + b);
+
+    return 0;
+}
