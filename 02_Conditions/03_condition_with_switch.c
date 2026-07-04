3ba6d07dd6e7e004a36d3c2bff28b2025fa9f2aa
Soumyajit Bala <soumyajitbala409@gmail.com>
2026-07-04 11:38:43 +0530

    Adding switch condition program

---
+++ 02_Conditions/03_condition_with_switch.c
@@ -0,0 +1,49 @@
+/*
+ * Program to display the day of the week using a switch statement.
+ */
+
+#include <stdio.h>
+
+int main() {
+
+    int day;
+
+    printf("Enter day (1-7): ");
+    scanf("%d", &day);
+
+    switch (day) {
+
+        case 1:
+            printf("Sunday\n");
+            break;
+
+        case 2:
+            printf("Monday\n");
+            break;
+
+        case 3:
+            printf("Tuesday\n");
+            break;
+
+        case 4:
+            printf("Wednesday\n");
+            break;
+
+        case 5:
+            printf("Thursday\n");
+            break;
+
+        case 6:
+            printf("Friday\n");
+            break;
+
+        case 7:
+            printf("Saturday\n");
+            break;
+
+        default:
+            printf("Invalid day number! Please enter a number between 1 and 7.\n");
+    }
+
+    return 0;
+}
