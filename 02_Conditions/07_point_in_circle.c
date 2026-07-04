f25e56399973c972232f8b557a2617a4a6ba69c0
Soumyajit Bala <soumyajitbala409@gmail.com>
2026-07-04 14:53:07 +0530

    Added program determins if the point lies in circle or not

---
+++ 02_Conditions/07_point_in_circle.c
@@ -0,0 +1,27 @@
+//This code determine whether a point lies inside the circle or not.
+#include<stdio.h>
+#include<math.h>
+int main() {
+    float xc, yc, r, x, y, distance;
+    printf("Input the center of the circle (xc, yc):");
+    scanf("%f %f", &xc, &yc);
+
+    printf("Input the radius of the circle (r):");
+    scanf("%f", &r);
+    
+    printf("Input the point coordinates (x, y):");
+    scanf("%f %f", &x, &y);
+
+    //Calculate the distance from the point to the center of the circle.
+    distance = sqrt((pow(x -xc, 2)) + (pow(y - yc, 2)));
+
+    if (distance < r) 
+        printf("The point is inside the circle.");
+    else if (distance == r)
+        printf("The point is on the circle.");
+    else 
+        printf("The point is outside the circle.");
+    
+    return 0;
+    
+}
