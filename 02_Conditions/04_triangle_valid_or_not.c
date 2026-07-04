29b66b169b011ac2920ad7674c2d8c429420ad4c
Soumyajit Bala <soumyajitbala409@gmail.com>
2026-07-04 11:57:56 +0530

    Adding program that determines if triangle valid or not

---
+++ 02_Conditions/04_triangle_valid_or_not.c
@@ -0,0 +1,20 @@
+/* 
+ Program to check whether a triangle is valid based on its angles
+*/
+
+#include <stdio.h>
+int main() {
+    int a, b, c;
+
+    printf("Enter the three angles separated by spaces: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b + c == 180 && a > 0 && b > 0 && c > 0) {
+        printf("The triangle is valid.\n");
+    }
+    else {
+        printf("The triangle is not valid.\n");
+    }
+
+    return 0;
+}
