4231ef48c1af949668fb3d2aef921b54f5c232c4
Soumyajit Bala <soumyajitbala409@gmail.com>
2026-07-12 21:38:18 +0530

    Program to determinal if input is even or odd

---
+++ 04_Functions/03_even_or_odd_using_function.c
@@ -0,0 +1,29 @@
+/*
+ * Program: Check whether a number is even or odd using a function.
+ * Description: Accepts an integer from the user and determines
+ *              whether it is even or odd using the even_or_odd() function.
+ */
+
+#include <stdio.h>
+int is_even(int n);
+
+int main() {
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if(is_even(n)) {
+        printf("Even\n");
+    }
+    else {
+        printf("Odd\n");
+    }
+
+    return 0;
+}
+
+int is_even(int n) {
+
+    return n %2 == 0;
+}
